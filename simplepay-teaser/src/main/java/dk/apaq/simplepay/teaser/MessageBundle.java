038c89d6b4d0233f58da13d868985b0c4868bedd
krog <krog@nkm17v85.nkd01100.nykreditnet.net>
2012-08-10 11:24:33 +0200

    More work on translation.

--- simplepay-teaser/src/main/java/dk/apaq/simplepay/teaser/MessageBundle.java
+++ simplepay-teaser/src/main/java/dk/apaq/simplepay/teaser/MessageBundle.java
@@ -1,7 +1,9 @@
 package dk.apaq.simplepay.teaser;
 
 import java.text.MessageFormat;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 /**
@@ -9,17 +11,30 @@ import java.util.ResourceBundle;
  * @author michael
  */
 public class MessageBundle {
+    private static final Map<String, MessageBundle> LOADED_BUNDLES = new HashMap<String, MessageBundle>();
     private Locale locale;
     private ResourceBundle bundle;
+    
 
-    public MessageBundle(Locale locale, String bundlePath) {
+    private MessageBundle(Locale locale, String bundlePath) {
         this.locale = locale;
         this.bundle = ResourceBundle.getBundle(bundlePath, locale);
     }
 
-    public String getMessage(String key, Object ... arguments) {
+    public String get(String key, Object ... arguments) {
         String pattern = bundle.getString(key);
         return MessageFormat.format(pattern, arguments);
+    }
+    
+    public static synchronized MessageBundle get(Locale locale, String bundlePath) {
+        String id = locale.toString()+":"+bundlePath;
+        MessageBundle bundle = LOADED_BUNDLES.get(id);
+        
+        if(bundle == null) {
+            bundle = new MessageBundle(locale, bundlePath);
+            LOADED_BUNDLES.put(id, bundle);
+        }
+        return bundle;
     }
     
 }
--- simplepay-teaser/src/main/java/dk/apaq/simplepay/teaser/site/TeaserPage.java
+++ simplepay-teaser/src/main/java/dk/apaq/simplepay/teaser/site/TeaserPage.java
@@ -1,5 +1,10 @@
 package dk.apaq.simplepay.teaser.site;
 
+import dk.apaq.simplepay.teaser.MessageBundle;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,7 +13,20 @@ import org.springframework.web.servlet.ModelAndView;
 public class TeaserPage {
     
     @RequestMapping(value="/index.htm")
-    public ModelAndView handleRequest() {
-        return new ModelAndView("teaser");
+    public ModelAndView handleRequest(HttpServletRequest request) {
+        MessageBundle message = MessageBundle.get(request.getLocale(), "dk.apaq.simplepay.teaser.i18n.Messages");
+        
+        String tooltip1 = message.get("tooltip1");
+        String tooltip2 = message.get("tooltip2");
+        
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("title", message.get("title"));
+        model.put("text1", message.get("text1", tooltip1));
+        model.put("text2", message.get("text2", tooltip2));
+        model.put("button", message.get("button"));
+        model.put("contact", message.get("contact"));
+        model.put("example", message.get("example"));
+        
+        return new ModelAndView("teaser", model);
     }
 }
