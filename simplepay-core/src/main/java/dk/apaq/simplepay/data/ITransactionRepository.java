df4c3f3df024dda0cf6347b334c6e47e36f2cbd3
michael <michael@michaels-imac.home>
2012-12-28 08:55:38 +0100

    Resolved conflicts after merge.

--- simplepay-core/src/main/java/dk/apaq/simplepay/data/ITransactionRepository.java
+++ simplepay-core/src/main/java/dk/apaq/simplepay/data/ITransactionRepository.java
@@ -11,12 +11,36 @@ import org.joda.money.Money;
  */
 public interface ITransactionRepository extends Repository<Transaction, String> {
     
+     /**
+     * Creates a New Transaction.
+     * @param token The token to use for the new transaction.
+     * @param refId The RefId to use for the transaction
+     * @param money The amount and currency the transaction should be authorized to handle.
+     * @return The new transaction.
+     */
     Transaction createNew(Token token, String refId, Money money);
     
+    /**
+     * Charges an existing transaction.
+     * @param transaction The transaction to charge.
+     * @param amount The amount to charge which must be equal to or less than the initial amount.
+     * @return The charged transaction.
+     */
     Transaction charge(Transaction transaction, long amount);
     
+    /**
+     * Cancel a transaction before it has been charged.
+     * @param transaction The transaction to cancel.
+     * @return The cancelled transaction.
+     */
     Transaction cancel(Transaction transaction);
     
+    /**
+     * Refunds a charged transaction.
+     * @param transaction The transaction to refund.
+     * @param amount The amount to refund which must equals to og less than the amount charged.
+     * @return The refunded transaction.
+     */
     Transaction refund(Transaction transaction, long amount);
     
 }
